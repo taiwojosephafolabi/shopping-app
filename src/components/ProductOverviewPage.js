a8bb99fd42fba7c03408bfce6571834d8d076998
Taiwo Joseph Afolabi <90561354+taiwojosephafolabi@users.noreply.github.com>
2023-05-10 22:54:05 +0000

    updates to overview page

--- src/components/ProductOverviewPage.js
+++ src/components/ProductOverviewPage.js
@@ -1,16 +1,18 @@
 import React from "react";
 import NavbarComp from "./NavbarComp";
 import Categories from "./Categories";
+import { productsData } from "../data/data";
 
 function ProductOverviewPage({
   backToLandingPage,
   showHomePage,
+  data,
   wishlistItem,
   setWishlistItem,
   cartItem,
   setCartItem,
   cartTotal,
-  setCartTotal
+  setCartTotal,
 }) {
   return (
     <div className="App">
@@ -27,9 +29,29 @@ function ProductOverviewPage({
 
       <Categories />
       <h1 className="heading">Product Overview</h1>
-      <button type="button" className="back-btn" onClick={() => showHomePage()}>
+      <span className="button-container">
+      <button type="button" className="button" onClick={() => showHomePage()}>
         Back to Home
       </button>
+      </span>
+      <div className="container">
+      {data
+        .filter((data) => {
+          return productsData.id === data.id
+            ? data
+            : data.id;
+        })
+        .map((data, index) => {
+          return (
+            <div className="liked-item" key={index}>
+              <h4> {data.name} </h4>
+              <img className="image" src={data.image} alt={data.name} />
+              <h6> Added on: {data.addedDate}</h6>
+              <h5> Price: £{data.price}</h5>
+              </div>
+          );
+        })}
+        </div>
     </div>
   );
 }
