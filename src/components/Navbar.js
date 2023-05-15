1177eb1d20eac0111b7996ea7d8c4b04230b24b1
Taiwo Joseph Afolabi <90561354+taiwojosephafolabi@users.noreply.github.com>
2023-05-15 23:19:56 +0000

    Navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import Cart from "./Cart";
 import Wishlist from "./Wishlist";
 
